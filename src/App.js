3ffaa8c624f4ba6f948dc1604284605e5479d2a6
SlimDridi01 <slimgotze01@gmail.com>
2021-11-17 14:39:55 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Profile from './profile/Profile'
 import './App.css';
+import me from './profile/me.jpg';
 
 function App() {
+  const btnalerte = () => {
+    alert('Slim Dridi');
+  }
   return (
-    <div >
-      <Profile FullName="Slim Dridi" bio="Ouardia" age="22 yo " profession="Gamer" />
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <Profile FullName="Slim Dridi" bio="Ouardia" age="22 yo " profession="Gamer" >
+        <img style={{ borderRadius: ' 50px', width: '30%' }} src={me} alt="My Pic" />
+      </Profile>
+      <button style={{ marginTop: '5px' }} onClick={btnalerte}> Button </button>
     </div>
   );
 }
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Photo from './Photo';
 
 
-function Profile({ FullName, bio, age, profession }) {
+
+function Profile(props) {
     return (
         <div style={{ textAlign: "center" }}>
-            <h1 style={{ backgroundColor: "grey", color: "white" }}> Hello my Name is <span style={{ color: "red" }}>  {FullName}  </span> . </h1>
-            <h3 > I live in {bio} i have {age} and i'm a <span style={{ color: "red", backgroundColor: "black" }}> {profession} </span></h3>
-            <Photo />
-
+            <h1 style={{ backgroundColor: "grey", color: "white" }}> Hello my Name is <span style={{ color: "red" }}>  {props.FullName}  </span> . </h1>
+            <h3 > I live in {props.bio} i have {props.age} and i'm a <span style={{ color: "red", backgroundColor: "black" }}> {props.profession} </span></h3>
+            {props.children}
         </div >
     )
 }
